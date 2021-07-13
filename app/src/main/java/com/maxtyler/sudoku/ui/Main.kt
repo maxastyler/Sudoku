107c69bc3246b3677b8e131da8978d328d0d8396
Max Tyler <maxastyler@gmail.com>
2021-07-13 20:17:52 +0100

    add animation stuff

--- app/src/main/java/com/maxtyler/sudoku/ui/Main.kt
+++ app/src/main/java/com/maxtyler/sudoku/ui/Main.kt
@@ -1,20 +1,42 @@
 package com.maxtyler.sudoku.ui
 
+import androidx.compose.animation.*
+import androidx.compose.animation.core.MutableTransitionState
+import androidx.compose.animation.core.calculateTargetValue
+import androidx.compose.foundation.gestures.awaitFirstDown
+import androidx.compose.foundation.gestures.horizontalDrag
+import androidx.compose.foundation.layout.offset
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.composed
+import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.input.pointer.positionChange
+import androidx.compose.ui.input.pointer.util.VelocityTracker
+import androidx.compose.ui.unit.IntOffset
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.navArgument
 import androidx.navigation.compose.rememberNavController
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.launch
+import kotlin.math.absoluteValue
+import kotlin.math.roundToInt
 
+@ExperimentalAnimationApi
 @Composable
 fun Main() {
     val navController = rememberNavController()
     NavHost(navController, "menu") {
         composable("menu") {
             val vm = hiltViewModel<MenuViewModel>()
-            Menu(vm, navController)
+            EnterAnimation {
+
+                Menu(vm, navController)
+            }
         }
         composable(
             "game/{gameId}/{numberOfClues}",
@@ -22,7 +44,83 @@ fun Main() {
                 navArgument("numberOfClues") { type = NavType.IntType })
         ) {
             val vm = hiltViewModel<SudokuViewModel>()
-            SudokuScreen(vm, { navController.navigateUp() })
+            EnterAnimation {
+
+                SudokuScreen(vm, { navController.navigateUp() })
+            }
         }
+    }
+}
+
+fun Modifier.swipeToDismiss(
+    onDismissed: () -> Unit
+): Modifier = composed {
+    val offsetX = remember { androidx.compose.animation.core.Animatable(0f) }
+    pointerInput(Unit) {
+        // Used to calculate fling decay.
+        val decay = splineBasedDecay<Float>(this)
+        // Use suspend functions for touch events and the Animatable.
+        coroutineScope {
+            while (true) {
+                // Detect a touch down event.
+                val pointerId = awaitPointerEventScope { awaitFirstDown().id }
+                val velocityTracker = VelocityTracker()
+                // Stop any ongoing animation.
+                offsetX.stop()
+                awaitPointerEventScope {
+                    horizontalDrag(pointerId) { change ->
+                        // Update the animation value with touch events.
+                        launch {
+                            offsetX.snapTo(
+                                offsetX.value + change.positionChange().x
+                            )
+                        }
+                        velocityTracker.addPosition(
+                            change.uptimeMillis,
+                            change.position
+                        )
+                    }
+                }
+                // No longer receiving touch events. Prepare the animation.
+                val velocity = velocityTracker.calculateVelocity().x
+                val targetOffsetX = decay.calculateTargetValue(
+                    offsetX.value,
+                    velocity
+                )
+                // The animation stops when it reaches the bounds.
+                offsetX.updateBounds(
+                    lowerBound = -size.width.toFloat(),
+                    upperBound = size.width.toFloat()
+                )
+                launch {
+                    if (targetOffsetX.absoluteValue <= size.width) {
+                        // Not enough velocity; Slide back.
+                        offsetX.animateTo(
+                            targetValue = 0f,
+                            initialVelocity = velocity
+                        )
+                    } else {
+                        // The element was swiped away.
+                        offsetX.animateDecay(velocity, decay)
+                        onDismissed()
+                    }
+                }
+            }
+        }
+    }
+        .offset { IntOffset(offsetX.value.roundToInt(), 0) }
+}
+
+
+@ExperimentalAnimationApi
+@Composable
+fun EnterAnimation(content: @Composable () -> Unit) {
+    val transitionState: MutableTransitionState<Boolean> =
+        remember { MutableTransitionState(false) }.also { it.targetState = true }
+    AnimatedVisibility(
+        visibleState = transitionState,
+        enter = fadeIn()
+    ) {
+        content()
     }
 }
