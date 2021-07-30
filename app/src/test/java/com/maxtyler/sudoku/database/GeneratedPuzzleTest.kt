b2ed1e098a6feafea680e8fa37b039e4961117aa
Max Tyler <maxastyler@gmail.com>
2021-07-30 22:19:51 +0100

    Add a uniqueness test

--- app/src/test/java/com/maxtyler/sudoku/database/GeneratedPuzzleTest.kt
+++ app/src/test/java/com/maxtyler/sudoku/database/GeneratedPuzzleTest.kt
@@ -2,8 +2,7 @@ package com.maxtyler.sudoku.database
 
 import com.maxtyler.sudoku.model.Solver
 import com.maxtyler.sudoku.model.Sudoku
-import junit.framework.Assert.assertNotNull
-import junit.framework.Assert.assertNotSame
+import junit.framework.Assert.*
 import kotlinx.collections.immutable.toPersistentMap
 import org.junit.Test
 
@@ -13,24 +12,33 @@ class GeneratedPuzzleTest {
         generatedPuzzle: GeneratedPuzzle,
         toRemove: Int
     ): Map<Pair<Int, Int>, String>? {
-        return Solver(Sudoku(generatedPuzzle.clues.take(9 * 9 - toRemove)
-            .map { (row, col, v) -> (row to col) to v.toInt() }
-            .toMap().toPersistentMap())).solve()
+        return generatedPuzzle.clues.take(9 * 9 - toRemove)
+            .map { (row, col, v) -> (row to col) to v }.toMap().let {
+                if (Solver.isUnique(it)) it else null
+            }?.mapValues { (_, v) -> v.toInt() }?.toPersistentMap()
+            ?.let { Solver(Sudoku(it)).solve() }
     }
 
     @Test
     fun shuffle() {
-        val toRemove = 40
+        val toRemove = 45
         var puzzle: List<Triple<Int, Int, String>>? = null
         while (puzzle == null) {
             Solver(Sudoku()).findMinSolutions(toRemove)?.let { puzzle = it }
         }
-        val p = GeneratedPuzzle(clues = puzzle!!, minimumClues = 9 * 9 - 3)
-        assertNotSame(p.shuffle(),p)
-        assertNotSame(p.shuffle(),p)
-        assertNotNull(solvePuzzle(p.shuffle(), toRemove))
-        assertNotNull(solvePuzzle(p.shuffle(), toRemove))
-        assertNotNull(solvePuzzle(p.shuffle(), toRemove))
-        assertNotNull(solvePuzzle(p.shuffle(), toRemove))
+        val p = GeneratedPuzzle(clues = puzzle!!, minimumClues = 9 * 9 - toRemove)
+        assertNotSame(p.shuffle(), p)
+        assertNotSame(p.shuffle(), p)
+        (0 until 100).forEach {
+            assertNotNull(solvePuzzle(p.shuffle(), toRemove))
+        }
+    }
+
+    @Test
+    fun testIsUnique() {
+        val p = Sudoku()
+        val solver = Solver(p).solve()
+        assertFalse(Solver.isUnique((0 until 4).flatMap { x -> (0 until 9).map { Pair(x, it) } }
+            .fold(solver!!, { acc, v -> acc + (v to "123456789") })))
     }
 }
